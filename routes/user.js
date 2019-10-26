d86d8dbeddfce43049c0217d10ae3f76f42b53c8
Melissa Alcantara <melissaalcantara02@gmail.com>
2019-10-26 18:18:09 -0400

    Creacion get all users

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user')
 
 // Get all users
 router.get('/', (req, res) => {
+    try {
+        const users = await User.find()
+        res.json(users)
+      } catch (err) {
+        res.status(500).json({ message: err.message })
+      }
 })
 
 // Get one user

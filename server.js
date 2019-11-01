64e6be3ed1f7fc487ce2a3c4be5193dca6bbd4f7
Melissa Alcantara <melissaalcantara02@gmail.com>
2019-11-01 08:47:25 -0400

    Comentarios sobre que hace el server.js

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+/**
+ * Incluyendo todas las dependencias que necesitamos para el proyecto
+ */
 require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
+/**
+ * Conectando a la base de datos
+ */
 mongoose.connect('mongodb://localhost:27017/icecream', { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
@@ -10,15 +16,22 @@ db.once('open', () => console.log('connected to database'))
 
 app.use(express.json())
 
-//configurando el get y el post del usuario
+/**
+ * Configurando el get y el post del usuario
+ */
 const userrouter = require('./routes/user')
 app.use('/user', userrouter)
 
-//configurando el get y el post de la lista
+/**
+ * Configurando el get y el post de la lista
+ */
+
 const listaordenrouter = require('./routes/listas')
 app.use('/listaorden', listaordenrouter)
 
-//configurando el get y el post de la orden
+/**
+ * Configurando el get y el post de la orden
+ */
 const ordenrouter = require('./routes/orden')
 app.use('/orden', ordenrouter)
 
